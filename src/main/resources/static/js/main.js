c26fca03f8d34e8a966dba204c9b15fca9f643b3
Galahad <nevezhindima@yandex.ru>
2022-02-15 11:05:12 +0300

    main фронт файлы

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import vueResource from 'vue-resource'
+import App from 'pages/App.vue'
+
+
+//const axios = require('axios').default;
+const app = createApp(App);
+app.mount('#app');
