45ce67ddb21172d0da4fb5cbd627c7b0ddc659ad
Galahad <nevezhindima@yandex.ru>
2022-02-15 11:06:53 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: path.join(__dirname, 'src', 'main', 'resources', 'static', 'js', 'main.js'),
+    devServer: {
+        static: './dist',
+        compress: true,
+        port: 8000,
+        allowedHosts: [
+            'localhost:8080'
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    resolve: {
+        modules: [
+            path.join(__dirname, 'src', 'main', 'resources', 'static', 'js'),
+            path.join(__dirname, 'node_modules'),
+        ],
+    }
+}
